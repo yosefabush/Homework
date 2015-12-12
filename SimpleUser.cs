34cfee6911b5d10afdcf02e1d7e44d783d19d067
master1590 <master010590@gmail.com>
2015-12-12 19:46:38 +0200

    Create SimpleUser.cs

---
+++ SimpleUser.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Homework_Project
+{
+	public class SimpleUser:User
+	{
+	
+        public SimpleUser():base("username","password","email")
+        {
+
+        }
+
+		public SimpleUser (string username ,string password, string email):base(username,password,email)
+		{
+		
+
+		}
+ 
+		public override string ToString(){
+			return base.ToString();
+		}
+
+	}
+}
+
