062781f1d0d9f8643cf3aa5a731349c0806c5b86
master1590 <master010590@gmail.com>
2015-12-12 19:47:18 +0200

    Create TeacherUser.cs

---
+++ TeacherUser.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Homework_Project
+{
+	public class TeacherUser:SimpleUser
+	{
+		public TeacherUser (string username ,string password, string email):base(username,password,email)
+		{
+
+
+		}
+
+
+
+
+
+		public override void Print(){
+			base.Print ();
+
+		}
+	}
+}
+
